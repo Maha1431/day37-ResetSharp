17e2719155a8e3b513d9d943e4d9dbb6fee4002f
maha <mmahaece07@gmail.com>
2021-12-17 18:54:55 +0530

    initial commit

---
+++ ResetSharp-/Api/Program.cs
@@ -0,0 +1,17 @@
+﻿using Reset_Api;
+using System;
+
+namespace Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            /*Response res = new Response();
+            res.Add("name:" |+ "ramu");
+            res.Delete(6)
+            res.Update("name:" + "emp.name" + "id:" + 7);*/ 
+        }
+     }
+}
---
+++ ResetSharp-/Api/Response.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reset_Api
+{
+    public class Response
+    {
+        public void getemp(string name)
+        {
+            RestClient restClient = new RestClient("http://localhost:4000");
+            RestRequest restRequest = new RestRequest("/emp", Method.GET);
+            var response = restClient.Execute(restRequest);
+            Console.WriteLine("Response :" + response.Content);
+        }
+
+        public void Delete(int id)
+        {
+            RestClient restClient = new RestClient("http://localhost:4000");
+            RestRequest restRequest = new RestRequest("/emp", Method.DELETE);
+            var response = restClient.Execute(restRequest);
+            Console.WriteLine("Response :" + response.Content);
+        }
+
+        public void Add(Emp emp)
+        {
+            RestClient restClient = new RestClient("http://localhost:4000");
+            RestRequest restRequest = new RestRequest("/emp", Method.POST);
+            restRequest.AddParameter("application/json", JsonConvert.SerializeObject(emp), ParameterType.RequestBody);
+            var response = restClient.Execute(restRequest);
+            Console.WriteLine("Response :" + response.Content);
+        }
+        public void Update(Emp emp, int id)
+        {
+            RestClient restClient = new RestClient("http://localhost:4000");
+            RestRequest restRequest = new RestRequest("/emp", Method.PUT);
+            restRequest.AddParameter("application/json", JsonConvert.SerializeObject(emp), ParameterType.RequestBody);
+            var response = restClient.Execute(restRequest);
+            Console.WriteLine("Response :" + response.Content); 
+        }
+    }
+}
